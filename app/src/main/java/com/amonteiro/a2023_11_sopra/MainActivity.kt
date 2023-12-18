4e64debb18bb196625ac04cf88571645eacc0aca
Anthony <anth06ny@gmail.com>
2023-12-18 14:37:28 +0100

    Correction Compose P2

--- app/src/main/java/com/amonteiro/a2023_11_sopra/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_11_sopra/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.amonteiro.a2023_11_sopra.ui.screens.SearchScreen
 import com.amonteiro.a2023_11_sopra.ui.theme.A2023_11_sopraTheme
 
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             A2023_11_sopraTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.LightGray) {
 
                     //Démarrage de l'application
                     SearchScreen()
--- app/src/main/java/com/amonteiro/a2023_11_sopra/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a2023_11_sopra/ui/screens/SearchScreen.kt
@@ -1,13 +1,30 @@
 package com.amonteiro.a2023_11_sopra.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -31,7 +48,7 @@ import com.bumptech.glide.integration.compose.placeholder
 @Composable
 fun SearchScreenPreview() {
     A2023_11_sopraTheme {
-        Surface(modifier = Modifier.fillMaxWidth(), color = Color.LightGray) {
+        Surface(modifier = Modifier.fillMaxWidth(), color = Color.Gray) {
             SearchScreen()
         }
     }
@@ -40,46 +57,110 @@ fun SearchScreenPreview() {
 //Composable représentant l'ensemble de l'écran
 @Composable
 fun SearchScreen() {
-    Column {
-        PictureRowItem(            data = pictureList[0])
+    Column(modifier = Modifier.padding(8.dp)) {
+
+        SearchBar()
+
+        Spacer(Modifier.size(8.dp))
+
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(pictureList.size) {
+                PictureRowItem(Modifier.background(Color.White), data = pictureList[it])
+            }
+        }
+
+        Row(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Clear",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Clear filter")
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Send,
+                    contentDescription = "Load data",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Load data")
+            }
+        }
+
+
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur par défaut
+        onValueChange = { newValue -> }, //Action
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        label = { Text("Enter text") }, //Texte d'aide qui se déplace
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
 }
 
 //Composable affichant 1 PictureData
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun PictureRowItem(modifier: Modifier = Modifier, data: PictureData) {
-   Row(modifier = modifier.fillMaxWidth()) {
-//Permission Internet nécessaire
-       GlideImage(
-           model = data.url,
-           //Dans string.xml
-           contentDescription = "Image",
-           //En dur
-           //contentDescription = "une photo de chat",
-           loading = placeholder(R.mipmap.ic_launcher_round), // Image de chargement
-           // Image d'échec. Permet également de voir l'emplacement de l'image dans la Preview
-           failure = placeholder(R.mipmap.ic_launcher),
-           contentScale = ContentScale.Fit,
-           //même autres champs qu'une Image classique
-           modifier = Modifier
-               .heightIn(max = 100.dp)
-               .widthIn(max= 100.dp)
-       )
+    Row(modifier = modifier.fillMaxWidth()) {
 
-       Column {
-           Text(
-               text = data.text,
-               fontSize = 20.sp,
-               textAlign = TextAlign.Center,
-               color = Color.Black
-           )
-           Text(
-               text = data.longText.take(20) + "...",
-               fontSize = 14.sp,
-               textAlign = TextAlign.Center,
-               color = Color.Blue
-           )
-       }
-   }
+//Permission Internet nécessaire
+        GlideImage(
+            model = data.url,
+            //Dans string.xml
+            contentDescription = "Image",
+            //En dur
+            //contentDescription = "une photo de chat",
+            loading = placeholder(R.mipmap.ic_launcher_round), // Image de chargement
+            // Image d'échec. Permet également de voir l'emplacement de l'image dans la Preview
+            failure = placeholder(R.mipmap.ic_launcher),
+            contentScale = ContentScale.Fit,
+            //même autres champs qu'une Image classique
+            modifier = Modifier
+                .heightIn(max = 100.dp)
+                .widthIn(max = 100.dp)
+        )
+
+        Column {
+            Text(
+                text = data.text,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black
+            )
+            Text(
+                text = data.longText.take(20) + "...",
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Blue
+            )
+        }
+    }
 }
